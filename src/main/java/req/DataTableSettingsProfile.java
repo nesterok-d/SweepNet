e0791f4124ee03b8467c1eeb8bea302a15541a01
nesterok-d <nesr.daria.nest@gmail.com>
2022-09-19 12:05:36 +0700

    added function for DataTableSettingsProfile part

--- src/main/java/req/DataTableSettingsProfile.java
+++ src/main/java/req/DataTableSettingsProfile.java
@@ -1,4 +1,14 @@
 package req;
 
-public class DataTableSettingsProfile {
+import res.ValidatableResponse;
+
+public class DataTableSettingsProfile extends Base{
+
+    public ValidatableResponse postDataTableSettingsProfile(String type){
+        return PostNoParams("profile/data-table-settings/"+ type, "");
+    }
+
+    public ValidatableResponse getDataTableSettingsProfile(String type){
+        return GetNoParams("profile/data-table-settings/"+ type);
+    }
 }
